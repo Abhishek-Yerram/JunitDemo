2e9d66dd07107d23b7804cfa666bbe6aa8cf90d4
Abhi <abhishekyerram02@gmail.com>
2021-02-18 14:38:56 +0530

    Committing

---
+++ src/main/java/com/valuemomentum/training/testing/JunitDemo/JUnitMessage.java
@@ -0,0 +1,16 @@
+package com.valuemomentum.training.testing.JunitDemo;
+
+public class JUnitMessage {
+	String m;
+	JUnitMessage(String msg){
+		m=msg;
+	}
+	String printMessage() {
+		return m;
+	}
+	String printHiMessage() {
+		return "Hi "+m;
+	
+	}
+
+}
